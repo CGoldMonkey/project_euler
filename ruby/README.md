945fd8a1d5b00c56115836c125686ffbd521eba7
Andrew <agstevens09@gmail.com>
2016-10-14 16:50:33 -0500

    Added some ruby versions to get the answers, changed script to try to work with JQuery on a button push

--- ruby/README.md
+++ ruby/README.md
@@ -1,7 +1,5 @@
 # project_euler
-Project Eurler Answers
-
-Edited the page to try to get it to work with JQuery with a button.
+Project Eurler Answers using Ruby
 
 Problem 1: Multiples of 3 and 5:
 	If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -22,3 +20,4 @@ Problem 3: Largest Prime Factor:
 	The prime factors of 13195 are 5, 7, 13 and 29.
 
 	What is the largest prime factor of the number 600851475143 ?
+	Need to tweak my methods for problem tree a little more for efficiency
--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+$(document).ready(function(){
+	$('#buttonType').on('click', function(){
+		problem1Query();
+
+
+	});
+});
+
+
+
 //Find the sum of all the multiples of 3 or 5 below 1000.
 //Function checks the input against the sum above and if 
 //true answers it is true else answers it is false
+
+
 function problem1() {
 	var sum = 0;
 	var inputStr = document.getElementById("problem1Answer").value;
@@ -20,4 +32,27 @@ function problem1() {
 		text = "Wrong, the answer is not "+input+", try again."
 		document.getElementById('guess').innerHTML = text;
 	}
+}
+
+
+function problem1Query() {
+	var sum = 0;
+	var input = $("#problem1Answer").val();
+	var answer = parseInt(input);
+	var text; 
+
+	for(var x = 0; x < 1000; x++){
+		if(x%3 === 0 || x%5 === 0){
+			sum += x;
+		}
+	}
+
+	if (answer === sum) {
+		text = "Correct, the answer is "+answer+".";
+		$('#guess').text(text);
+	} else {
+		text = "Wrong, the answer is not "+answer+", try again."
+		$('#guess').text(text);
+	}
+
 }
